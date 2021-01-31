6596d37f3d75460f69c3c59467a4fe4ca1bd744a
MohiuddinRabby <rabbymohiuddin@gmail.com>
2021-01-31 20:50:11 +0600

    first commit

---
+++ src/app/components/findCountry/Api/api.js
@@ -0,0 +1,11 @@
+import Axios from "axios";
+export const getCountryList = async (setter) => {
+  try {
+    const res = await Axios.get(`https://restcountries.eu/rest/v2/all`);
+    if (res.status === 200 && res?.data) {
+      setter(res?.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
